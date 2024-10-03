02587dad84cb475892ed9d8ce490d664a182b594
Anton Miroshnochenko <soooyc@gmail.com>
2024-10-03 11:35:22 +0300

    first commit

---
+++ basics/1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cats (
+  name VARCHAR(50),
+  age INT
+);
+
+CREATE TABLE dogs (
+  name VARCHAR(50),
+  breed VARCHAR(50),
+  age INT
+);
+
+SHOW COLUMNS FROM cats;
+SHOW COLUMNS FROM dogs;
